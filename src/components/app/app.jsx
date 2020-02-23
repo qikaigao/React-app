1e6623f19009e5154c2634a0eb66fa727a4097cc
qikaigao <qikaigao2017@gmail.com>
2020-02-22 21:24:56 -0800

    add comment

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,7 +3,37 @@ import CommentAdd from "../comment-add/comment-add";
 import CommentList from "../comment-list/comment-list";
 
 class App extends Component{
+
+    //  // 给组件对象指定state属性
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {
+    //         comments: [
+    //             {username:'Tom', content:'React挺好的！'},
+    //             {username:'Jack', content:'React太难了！'},
+    //             {username:'Ross', content:'React so easy！'},
+    //         ]
+    //     }
+    // }
+
+    state = {
+        comments: [
+            {username:'Tom', content:'React挺好的！'},
+            {username:'Jack', content:'React太难了！'},
+            {username:'Ross', content:'React so easy！'},
+        ]
+    }
+
+    addComment = (comment) => {
+        const {comments} = this.state;
+        comments.unshift(comment)
+        // 更新状态
+        this.setState({comments})
+    }
+
     render() {
+        const {comments} = this.state;
         return (
             <div>
                 <header className='site-header jumbotron'>
@@ -17,8 +47,8 @@ class App extends Component{
                 </header>
                 <div className='container'>
                     <div className='row'>
-                        <CommentAdd />
-                        <CommentList />
+                        <CommentAdd addComment={this.addComment}/>
+                        <CommentList comments={comments}/>
                     </div>
 
                 </div>
--- src/components/comment-list/comment-list.jsx
+++ src/components/comment-list/comment-list.jsx
@@ -1,38 +1,36 @@
 import React,{Component} from "react";
+import PropTypes from 'prop-types';
 import './comment-list.css';
 
+import CommentItem from "../comment-item/comment-item";
+
 class CommentList extends Component{
+
+    static propTypes = {
+        comments: PropTypes.array.isRequired
+    }
+
+
     render() {
+        const {comments} = this.props
         return (
             <div className='col-md-8'>
                 <h3 className='reply'>评论回复: </h3>
                 <h2 className={{display:'none'}}>暂无评论，点击左侧添加评论!!!</h2>
                 <ul className='list-group'>
-                    <li className='list-group-item'>
-                        <div className='handle'>
-                            <a href='#'>删除</a>
-                            <p className='user'><span>xxx</span> <span>说:</span></p>
-                            <p className='centence'>React不错</p>
-                        </div>
-                    </li>
-                    <li className='list-group-item'>
-                        <div className='handle'>
-                            <a href='#'>删除</a>
-                            <p className='user'><span>yyy</span> <span>说:</span></p>
-                            <p className='centence'>React有点难</p>
-                        </div>
-                    </li>
-                    <li className='list-group-item'>
-                        <div className='handle'>
-                            <a href='#'>删除</a>
-                            <p className='user'><span>zzz</span> <span>说:</span></p>
-                            <p className='centence'>React太难了</p>
-                        </div>
-                    </li>
+                    {
+                        comments.map((c,i)=><CommentItem comment={c} key={i} />)
+                    }
                 </ul>
             </div>
         );
     };
 }
+
+//  // 这个写法很蠢
+// CommentList.propTypes = {
+//     comments:PropTypes.array.isRequired
+//
+// }
 
 export default CommentList;
