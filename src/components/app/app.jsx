f735b4e20a67a96c489f69c723feb140c12dab71
qikaigao <qikaigao2017@gmail.com>
2020-02-24 11:57:57 -0800

    Ant Design Mobile UI

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,40 +1,16 @@
 import React,{Component} from "react";
-import {Switch,Route,Redirect} from 'react-router-dom';
-import About from '../../views/about/about';
-import Home from '../../views/home/home';
-import MyNavLink from '../my-nav-link/my-nav-link';
+import { Button, Toast } from 'antd-mobile';
 
 class App extends Component{
+
+    handleClick = () => {
+        Toast.info('提交成功', 1);
+    };
+
     render() {
         return (
-            <div className='container'>
-                <div className='row justify-content-xs-center'>
-                    <div className='col-xs-8 col'>
-                        <div className='pb-2 mt-4 mb-2 border-bottom'><h2>React Router Demo</h2></div>
-                    </div>
-                </div>
-
-                <div className='row'>
-                    <div className='col-xs-2 offset-xs-2'>
-                        <div className='list-group' >
-                            <MyNavLink className='list-group-item' to='/about' >About</MyNavLink>
-                            <MyNavLink className='list-group-item ' to='/home' >Home</MyNavLink>
-                        </div>
-                    </div>
-                    <div className='col-xs-6 col'>
-                        <div className='card'>
-                            <div className='card-body'>
-                                <Switch>
-                                    <Route path="/about"><About /></Route>
-                                    <Route path="/home"><Home /></Route>
-                                    <Redirect to="/home"/>
-                                </Switch>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
+            <div>
+                <Button type='primary' onClick={this.handleClick}>Start</Button>
             </div>
         )
     };
